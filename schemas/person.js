3ee03b6e567e884562fac92afe3ca0d9475e2370
binarytrance <ganeshan.dash@gmail.com>
2020-12-22 16:22:40 +0530

    added the people schema

---
+++ schemas/person.js
@@ -0,0 +1,37 @@
+import {MdPerson as icon} from 'react-icons/md';
+export default {
+    // computer name
+    name: 'person',
+    // visible title
+    title: 'Person',
+    icon: icon,
+    type: 'document',
+    fields: [
+        {
+            name: 'name',
+            title: 'Slice Master', // this is human readable
+            type: 'string',
+        },
+        {
+            name: 'slug',
+            title: 'Slug', // this is human readable
+            type: 'slug',
+            description: 'Slugified text',
+            options: {
+                source: 'name',
+                maxLength: 100
+            }
+        },
+        {
+            name: 'description',
+            title: 'Description', // this is human readable
+            type: 'text',
+            description: 'Tell us about the person',
+        },
+        {
+            name: 'image',
+            title: 'Image', // this is human readable
+            type: 'image',
+        }
+    ]
+}
